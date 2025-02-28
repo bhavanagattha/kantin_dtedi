b9f347b6a5e40f78dfa880ad064e1558a35f0688
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2025-02-28 17:53:34 +0700

    add : add unique for username column

--- database/migrations/2024_11_01_085258_create_cashier_table.php
+++ database/migrations/2024_11_01_085258_create_cashier_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('cashier', function(Blueprint $table){
             $table->id();
             $table->string('name');
-            $table->string('username');
+            $table->string('username') -> unique();
             $table->string('password');
             $table->integer('no_telp');
             $table->integer('salary');
